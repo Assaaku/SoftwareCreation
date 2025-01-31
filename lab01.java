3a3f7cef43e349db523389d270c4f8be10df0b9b
Assaaku <110076008+Assaaku@users.noreply.github.com>
2025-01-31 15:23:03 +0800

    1. Initial commit
    Commit with print declared

---
+++ lab01.java
@@ -0,0 +1,12 @@
+public class lab01 {
+    
+    static public void print(Object input1){
+        System.out.println(input1);
+    }
+    public static void main(String[] args){
+        int intValue = 0;
+        print(intValue);
+    }
+    
+}
+//THUS I SHALL NOT HAVE TO MANUALLY WRITE PRINT ALL THE TIME MWHAHAHHAHAHA
