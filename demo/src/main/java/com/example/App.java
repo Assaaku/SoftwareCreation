873c9b7b69027792caab832e7899b377a60a8d54
Assaaku <110076008+Assaaku@users.noreply.github.com>
2025-02-06 19:31:13 +0800

    shit works

---
+++ demo/src/main/java/com/example/App.java
@@ -0,0 +1,20 @@
+package com.example;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    static public void print(Object input1){
+        System.out.println(input1);
+    }
+    public static void main(String[] args){
+            int intValue = 0;
+            for(int i =0; i<10; i++){          
+                  print("HeyHello IM BAYRAAAAAAAAAA" + intValue);    
+                          print("HeyHello IM BAYRAAAAAAAAAA" + intValue);
+            }
+        }
+    
+}
--- lab01.java
+++ lab01.java
@@ -3,13 +3,13 @@ public class lab01 {
     static public void print(Object input1){
         System.out.println(input1);
     }
-    public static void main(String[] args){
-        int intValue = 0;
-        for(int i =0; i<10; i++){
-            print("HeyHello IM BAYRAAAAAAAAAA" + intValue);
-            print("HeyHello IM BAYRAAAAAAAAAA" + intValue);
-        }
-    }
+    // public static void main(String[] args){
+    //     int intValue = 0;
+    //     for(int i =0; i<10; i++){          
+    //           print("HeyHello IM BAYRAAAAAAAAAA" + intValue);    
+    //                   print("HeyHello IM BAYRAAAAAAAAAA" + intValue);
+    //     }
+    // }
     
 }
 //THUS I SHALL NOT HAVE TO MANUALLY WRITE PRINT ALL THE TIME MWHAHAHHAHAHA
